82e0875dee4adbf339574370493cc5b2eb7dcee6
Alabere-coder <alabere4real247@gmail.com>
2022-10-24 14:21:52 +0100

    header

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-        ('window-scrolled'), window.scrollY > 0
+        ('window-scrolled', window.scrollY > 0)
 })
