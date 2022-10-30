94ad8c27203a403387c75480501d4a793ded19f0
Alabere-coder <alabere4real247@gmail.com>
2022-10-30 15:10:32 +0100

    floating-link

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
         ('window-scrolled', window.scrollY > 0)
 })
+
+
+const textBtns = document.querySelectorAll('.contact__btn');
+
+textBtns.forEach(textBtn => {
+    let text = textBtn.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+})
